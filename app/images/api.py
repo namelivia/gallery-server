57b85d1671f6e0eec236fc9f8b7496b47e8608c4
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-27 21:41:54 +0200

    Boto3 work in progress

--- app/images/api.py
+++ app/images/api.py
@@ -3,10 +3,18 @@ from app.dependencies import get_db
 from . import schemas
 from typing import List
 from sqlalchemy.orm import Session
+import boto3
+import os
 
 router = APIRouter(prefix="/images", dependencies=[Depends(get_db)])
 
 
 @router.get("", response_model=List[schemas.Image])
 def images(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
+    # WIP
+    # client = boto3.client('s3', os.getenv('AWS_REGION'))
+    # paginator = client.get_paginator('list_objects')
+    # page_iterator = paginator.paginate(Bucket=os.getenv('AWS_BUCKET'))
+    # for page in page_iterator:
+    #    print(page['Contents'])
     return [schemas.Image(url='http://foo.bar')]
--- scripts/scheduler.py
+++
@@ -1,8 +0,0 @@
-from app.tasks.tasks import Tasks
-from app.database import engine, Base
-from app.dependencies import get_db
-
-Base.metadata.create_all(bind=engine)
-db = next(get_db())
-
-Tasks.send_watering_reminders(db)

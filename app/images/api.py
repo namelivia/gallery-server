92a1fc0e8b4cc33de9ac71f4d710cbd8912af60e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-28 20:17:23 +0200

    Serve images from s3 bucket

--- app/images/api.py
+++ app/images/api.py
@@ -3,18 +3,54 @@ from app.dependencies import get_db
 from . import schemas
 from typing import List
 from sqlalchemy.orm import Session
+import logging
 import boto3
 import os
 
 router = APIRouter(prefix="/images", dependencies=[Depends(get_db)])
 
+logger = logging.getLogger(__name__)
+
 
 @router.get("", response_model=List[schemas.Image])
-def images(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
-    # WIP
-    # client = boto3.client('s3', os.getenv('AWS_REGION'))
-    # paginator = client.get_paginator('list_objects')
-    # page_iterator = paginator.paginate(Bucket=os.getenv('AWS_BUCKET'))
-    # for page in page_iterator:
-    #    print(page['Contents'])
-    return [schemas.Image(url='http://foo.bar')]
+def images(db: Session = Depends(get_db), page: int = 0):
+    page_size = 9
+
+    # Retrieve data
+    client = boto3.client('s3', os.getenv('AWS_REGION'))
+    paginator = client.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=os.getenv('AWS_BUCKET'))
+
+    # Filter data
+    all_items = []
+    for s3_page in page_iterator:
+        items = s3_page['Contents']
+        for item in items:
+            if item['Key'].endswith('.jpg'):
+                all_items.append(item)
+
+    all_items = sorted(
+        all_items,
+        key=lambda i: i['LastModified'],
+        reverse=True
+    )
+
+    # Get only needed items
+    result = []
+    start = page * page_size
+    end = start + page_size
+    for item in all_items[start:end]:
+        try:
+            signed_url = client.generate_presigned_url(
+                'get_object',
+                Params={
+                    'Bucket': os.getenv('AWS_BUCKET'),
+                    'Key': item['Key']
+                },
+                ExpiresIn=3600
+            )
+        except Exception:
+            pass
+        image = schemas.Image(url=signed_url, date=item['LastModified'])
+        result.append(image)
+    return result
--- app/images/schemas.py
+++ app/images/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
+import datetime
 
 
 class Image(BaseModel):
     url: str = Field(title="Image url")
+    date: datetime.datetime = Field(title="Image last modified")

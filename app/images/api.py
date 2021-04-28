714dd8708233668a67e5cb9ca832e33b84fe003f
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-28 21:00:49 +0200

    Fix linting

--- app/images/api.py
+++ app/images/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from app.dependencies import get_db
+from sqlalchemy.orm import Session
 from . import schemas
 from typing import List
-from sqlalchemy.orm import Session
 import logging
 import boto3
 import os
@@ -17,23 +17,19 @@ def images(db: Session = Depends(get_db), page: int = 0):
     page_size = 9
 
     # Retrieve data
-    client = boto3.client('s3', os.getenv('AWS_REGION'))
-    paginator = client.get_paginator('list_objects_v2')
-    page_iterator = paginator.paginate(Bucket=os.getenv('AWS_BUCKET'))
+    client = boto3.client("s3", os.getenv("AWS_REGION"))
+    paginator = client.get_paginator("list_objects_v2")
+    page_iterator = paginator.paginate(Bucket=os.getenv("AWS_BUCKET"))
 
     # Filter data
     all_items = []
     for s3_page in page_iterator:
-        items = s3_page['Contents']
+        items = s3_page["Contents"]
         for item in items:
-            if item['Key'].endswith('.jpg'):
+            if item["Key"].endswith(".jpg"):
                 all_items.append(item)
 
-    all_items = sorted(
-        all_items,
-        key=lambda i: i['LastModified'],
-        reverse=True
-    )
+    all_items = sorted(all_items, key=lambda i: i["LastModified"], reverse=True)
 
     # Get only needed items
     result = []
@@ -42,15 +38,12 @@ def images(db: Session = Depends(get_db), page: int = 0):
     for item in all_items[start:end]:
         try:
             signed_url = client.generate_presigned_url(
-                'get_object',
-                Params={
-                    'Bucket': os.getenv('AWS_BUCKET'),
-                    'Key': item['Key']
-                },
-                ExpiresIn=3600
+                "get_object",
+                Params={"Bucket": os.getenv("AWS_BUCKET"), "Key": item["Key"]},
+                ExpiresIn=3600,
             )
         except Exception:
             pass
-        image = schemas.Image(url=signed_url, date=item['LastModified'])
+        image = schemas.Image(url=signed_url, date=item["LastModified"])
         result.append(image)
     return result
